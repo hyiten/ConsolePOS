bf07f8162c0b59584281b61b0a5c3fe947addf9a
hyiten <85594858+hyiten@users.noreply.github.com>
2021-08-06 10:59:52 +0800

    update
    updated 6/8

--- main.js
+++ main.js
@@ -5,52 +5,57 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-for (i = 0; i < menuitems.length; i++) {
-  console.log(`${i} - ${menuitems[i].name}`);
+function MenuSelection() {
+  
+  for (i = 0; i < menuitems.length; i++) {
+    console.log(`${i} - ${menuitems[i].name}`);
+  }
+
+  rl.question('What would you like to order ?', (category) => {
+      if (!isNaN(category)) {
+          ItemSelection(category)
+      } else {
+          MenuSelection()
+      }
+  })
 }
 
-rl.question("What would you like to order ? ", function (category) {
+function ItemSelection(category) {
+  
   for (j = 0; j < menuitems[parseInt(category)].items.length; j++) {
     console.log(`${j} - ${menuitems[parseInt(category)].items[j].name}`);
   }
-  rl.question(
-    `Which ${menuitems[category].name} would you like to order ? `,
-    function (item) {
-      for (k = 0; k < menuitems[category].items[item].options.length; k++) {
-        console.log(
-          `${k} - ${menuitems[category].items[item].options[k].name}`
-        );
+
+  rl.question(`Which ${menuitems[category].name} would you like to order ?`, (item) => {
+      if (!isNaN(item)) {
+        OptionSelection(category,item)
+      } else {
+        ItemSelection()
       }
-      rl.question(
-        `How would you like your ${menuitems[category].items[item].name} ? `,
-        function (preference) {
-          try {
-            for (
-              l = 0;
-              l < menuitems[category].items[item].options2.length;
-              l++
-            ) {
-              console.log(
-                `${l} - ${menuitems[category].items[item].options2[l].name}`
-              );
-            }
-            rl.question(
-              `Your order for "${menuitems[category].items[item].options[preference].name} ${menuitems[category].items[item].name}" ? `,
-              function (item2) {
-                console.log(`"${menuitems[category].items[item].options2[item2].name} ${menuitems[category].items[item].options[preference].name} ${menuitems[category].items[item].name}"`)                  
-                rl.close();
-              }
-            );
-          } catch (err) {
-            // no-addons
-            console.log(`"${menuitems[category].items[item].options[preference].name} ${menuitems[category].items[item].name}"`)
-            rl.close();
-          }
-        }
-      );
-    }
-  );
-});
+  })
+}
+
+function OptionSelection(category,item) {
+  
+  for (k = 0; k < menuitems[category].items[item].options.length; k++) {
+    console.log(
+      `${k} - ${menuitems[category].items[item].options[k].name}`
+    );
+  }
+
+  rl.question('What is your selection>', (answer) => {
+      if (!isNaN(answer)) {
+          console.log('You typed: ', answer)
+          //send it to the ShoppingCart
+          rl.close()
+      } else {
+        OptionSelection()
+      }
+  })
+}
+
+MenuSelection()
+
 
 rl.on("close", function () {
   console.log("\nBYE BYE !!!");
