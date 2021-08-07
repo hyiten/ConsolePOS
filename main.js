fe514b8603192119adacba7dea267d3cfaab1ea1
hyiten <85594858+hyiten@users.noreply.github.com>
2021-08-08 02:32:34 +0800

    updated

--- main.js
+++ main.js
@@ -1,83 +1,125 @@
 const { menuitems } = require("./offerings");
-const currentTime = require("./timestampFnc.js")
+const currentTime = require("./timestampFnc.js");
+const shoppingCart = require("./shoppingCart.js");
 const readline = require("readline");
+const uuid = require("uuid");
+
+trxid = uuid.v4();
+
 var orderPrice = 0;
-var currentOrder = []
+var currentOrder = [];
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 function MenuSelection() {
-  
   for (i = 0; i < menuitems.length; i++) {
     console.log(`${i} - ${menuitems[i].name}`);
   }
 
-  rl.question(`What would you like to order [${menuitems.length}]?`, (category) => {
+  rl.question(
+    `What would you like to order [${menuitems.length}]?`,
+    (category) => {
       if (parseInt(category) < menuitems.length) {
-          ItemSelection(category)
+        ItemSelection(category);
       } else {
-          console.log('please enter only 0 -', menuitems.length-1);
-          MenuSelection()
+        console.log("please enter only 0 -", menuitems.length - 1);
+        MenuSelection();
       }
-  })
+    }
+  );
 }
 
 function ItemSelection(category) {
-  
   for (j = 0; j < menuitems[parseInt(category)].items.length; j++) {
-    console.log(`${j} - ${menuitems[parseInt(category)].items[j].name} / ${menuitems[parseInt(category)].items[j].price}`);
+    console.log(
+      `${j} - ${menuitems[parseInt(category)].items[j].name} / ${
+        menuitems[parseInt(category)].items[j].price
+      }`
+    );
   }
 
-  rl.question(`Which ${menuitems[category].name} would you like to order ?`, (item) => {
+  rl.question(
+    `Which ${menuitems[category].name} would you like to order ?`,
+    (item) => {
       if (parseInt(item) < menuitems[parseInt(category)].items.length) {
         orderPrice = menuitems[parseInt(category)].items[parseInt(item)].price;
-        OptionSelection(category,item)
+        OptionSelection(category, item);
       } else {
-        ItemSelection(category)
+        ItemSelection(category);
       }
-  })
+    }
+  );
 }
 
-function OptionSelection(category,item) {
-  
+function OptionSelection(category, item) {
   for (k = 0; k < menuitems[category].items[item].options.length; k++) {
     console.log(
       `${k} - ${menuitems[category].items[item].options[k].name} / ${menuitems[category].items[item].options[k].addprice}`
     );
   }
 
-  rl.question('What is your selection>', (answer) => {
-      if (parseInt(answer) < menuitems[category].items[item].options.length) {
-          orderPrice = orderPrice + menuitems[category].items[item].options[parseInt(answer)].addprice;
-          console.log('You have ordered ', menuitems[parseInt(category)].items[parseInt(item)].name, '/', menuitems[category].items[item].options[parseInt(answer)].name, '/ cost ', orderPrice);
-          //ask the user if the order is all they want or they want to add items into the order
-          currentOrder.push(`${menuitems[parseInt(category)].name},${menuitems[parseInt(category)].items[parseInt(item)].name},${menuitems[category].items[item].options[parseInt(answer)].name},${orderPrice}`);
+  rl.question("What is your selection>", (answer) => {
+    if (parseInt(answer) < menuitems[category].items[item].options.length) {
+      orderPrice =
+        orderPrice +
+        menuitems[category].items[item].options[parseInt(answer)].addprice;
+      console.log(
+        "You have ordered ",
+        menuitems[parseInt(category)].items[parseInt(item)].name,
+        "/",
+        menuitems[category].items[item].options[parseInt(answer)].name,
+        "/ cost ",
+        orderPrice
+      );
+      //ask the user if the order is all they want or they want to add items into the order
+      currentOrder.push(
+        `${menuitems[parseInt(category)].name},${
+          menuitems[parseInt(category)].items[parseInt(item)].name
+        },${
+          menuitems[category].items[item].options[parseInt(answer)].name
+        },${orderPrice}`
+      );
 
-          rl.question('Is that all (Y/N)?', (answer) => {
-            if (answer == 'y') {
-              for (listOrder = 0; listOrder < currentOrder.length; listOrder++) {
-                console.log(`${currentTime.convert(currentTime.get())} --> ${currentOrder[listOrder]}`)
-              }
-              console.log('Your order is complete. [done]')
-              rl.close();
-            } else {
-              console.log('Ok, let me add some more to your order')
-              MenuSelection()
-            }
-          })
+      details = `${menuitems[parseInt(category)].name}/${
+        menuitems[parseInt(category)].items[parseInt(item)].name
+      }/${menuitems[category].items[item].options[parseInt(answer)].name}`;
+      price = orderPrice;
 
-      } else {
-        OptionSelection(category,item)
-      }
-  })
+      //write order into sqlite database
+      shoppingCart.add(trxid, details, 1, price, currentTime.get());
+
+      rl.question("Is that all (Y/N)?", (answer) => {
+        if (answer == "y") {
+          console.log('')
+          console.log('Your orders are')
+          console.log('----------------')
+          grandTotal = 0
+          for (allOrders = 0; allOrders < currentOrder.length; allOrders++) {
+            orderDetails = currentOrder[allOrders].split(",");
+            grandTotal += parseFloat(orderDetails[3])
+            console.log(`${allOrders +1} - ${orderDetails[0]}/${orderDetails[1]}/${orderDetails[2]} .......... x1 ($${orderDetails[3]})`);
+          }
+          console.log('----------------')
+          console.log(`GRAND Total : $${grandTotal}`)
+         
+          rl.close();
+        } else {
+          console.log("Ok, let me add some more to your order");
+          MenuSelection();
+        }
+      });
+    } else {
+      OptionSelection(category, item);
+    }
+  });
 }
 
-MenuSelection()
-
+MenuSelection();
 
 rl.on("close", function () {
+  //show receipt
   console.log("\nBYE BYE !!!");
   process.exit(0);
 });
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,8 +1,54 @@
-exports.shoppingCart.init = function () {
-    var cart = []
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("consolepos.db");
+
+function get(id) {
+    db.run(
+        `SELECT * from Orders WHERE ID = (?)`,
+        [id],
+        function (err) {
+            if (err) {
+                console.log(`error reading from db ${err}`);
+                OrderID = {}
+            } else {
+                console.log(`found [${this.ID}`);
+                OrderID = {id: this.ID, trxid: this.TransactionID, details: this.Details, qty: this.Qty, price: this.Price, ordertime: this.OrderTime}
+            }
+            return OrderID
+        }
+      );    
 }
 
-exports.shoppingCart.addItem = function (category, item) {
-    cart.addItem(category, item)
-    return cart
+function add(trxid, details, qty, price, ordertime) {
+  db.run(
+    `INSERT into Orders(TransactionID,Details,Qty,Price,OrderTime) VALUES (?,?,?,?,?)`,
+    [trxid, details, qty, price, ordertime],
+    function (err) {
+        if (err) {
+            console.log(`error writing to db ${err}`);
+            Success = false
+        } else {
+            Success = true;
+        }
+        return Success
+    }
+  );
 }
+
+function del(id) {
+    db.run(
+        `DELETE FROM Orders WHERE ID = (?)`,
+        [id],
+        function (err) {
+            if (err) {
+                console.log(`error deleting from db ${err}`);
+                Success = false
+            } else {
+                console.log(`Deleted [${id}`);
+                Success = true
+            }
+            return Success
+        }
+      );
+}
+
+module.exports = { get, add, del };
